461c3485e0c2b6de21698b18a78c1c5e51cd8f24
keisuke <keisuke.suzuki46@gmail.com>
2022-05-26 23:05:33 +0900

    Add panic

--- src/main.go
+++ src/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/pion/stun"
 )
 
-
 func clickTest(_ js.Value, _ []js.Value) interface{} {
 	dom.SetValue("result", "innerText", "Clicked")
 	return nil
@@ -19,7 +18,8 @@ func createStunRequest(_ js.Value, _ []js.Value) interface{} {
 	c, err := stun.Dial("udp", "stun.webrtc.ecl.ntt.com:3478")
 	if err != nil {
 		dom.SetValue("result", "innerText", fmt.Sprintln("STUN dial faild:", err.Error()))
-        dom.AddClass("result", "error")
+		dom.AddClass("result", "error")
+		panic(fmt.Sprintln("STUN dial faild:", err.Error()))
 	}
 	// Building binding request with random transaction id.
 	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
@@ -27,27 +27,30 @@ func createStunRequest(_ js.Value, _ []js.Value) interface{} {
 	if err := c.Do(message, func(res stun.Event) {
 		if res.Error != nil {
 			dom.SetValue("result", "innerText", err.Error())
-            dom.AddClass("result", "error")
+			dom.AddClass("result", "error")
+			panic(fmt.Sprintln("res.Error:", err.Error()))
 		}
 		// Decoding XOR-MAPPED-ADDRESS attribute from message.
 		var xorAddr stun.XORMappedAddress
 		if err := xorAddr.GetFrom(res.Message); err != nil {
 			dom.SetValue("result", "innerText", fmt.Sprintln("xorAddr FAILED!!:", err.Error()))
-            dom.AddClass("result", "error")
+			dom.AddClass("result", "error")
+			panic(fmt.Sprintln("xorAddr FAILED!!:", err.Error()))
 		}
-        dom.SetValue("result", "innerText", fmt.Sprintln("your IP is", xorAddr.IP))
+		dom.SetValue("result", "innerText", fmt.Sprintln("your IP is", xorAddr.IP))
 	}); err != nil {
 		dom.SetValue("result", "innerText", fmt.Sprintln("ERROR in c.Do()!!:", err.Error()))
-        dom.AddClass("result", "error")
+		dom.AddClass("result", "error")
+		panic(fmt.Sprintln("ERROR in c.Do()!!:", err.Error()))
 	}
-    return nil
+	return nil
 }
 
 func main() {
 	dom.Hide("loading")
 	dom.AddEventListener("run-button", "click", createStunRequest)
 
+	fmt.Println("Program has started!")
 	ch := make(chan struct{})
 	<-ch
-	fmt.Println("Program has started!")
 }
